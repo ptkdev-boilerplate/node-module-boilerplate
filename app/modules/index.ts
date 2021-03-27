5eb09e9f42c98853f17d7836d54e40fb47ff7cbd
Patryk Rzucidlo (PTKDev) <ptkdev@gmail.com>
2021-03-27 01:36:16 +0100

    [Release] v1.0.20210326

---
+++ app/modules/index.ts
@@ -0,0 +1,14 @@
+/**
+ * WebComponent Script
+ * =====================
+ *
+ * @contributors: Patryk Rzucidło [@ptkdev] <support@ptkdev.io> (https://ptk.dev)
+ *
+ * @license: MIT License
+ *
+ */
+export default function a(): number {
+	return 22222;
+}
+
+console.log(a());
